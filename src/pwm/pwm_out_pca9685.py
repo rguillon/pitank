b379f28537c95feacdd90041819902f6934a2db6
renaud <renaud.guillon@gmail.com>
2019-12-04 14:08:37 +0000

    Wip

--- src/pwm/pwm_out_pca9685.py
+++ src/pwm/pwm_out_pca9685.py
@@ -11,7 +11,7 @@ from .pwm_out_itf import pwm_out_itf
 # Initialise the PCA9685 using the default address (0x40).
 pca9685 = Adafruit_PCA9685.PCA9685(busnum=0)
 # Set frequency to 60hz, good for servos.
-pca9685.set_pwm_freq(60)
+pca9685.set_pwm_freq(1000)
 
 
 
@@ -24,4 +24,4 @@ class pwm_out(pwm_out_itf):
 
 
     def set(self, value: float):
-        self.pwm.set_pwm(self.out_num, 0, int(value * 4096.0))
+        self.pwm.set_pwm(self.out_num, 0, int(value * 4095.0))
